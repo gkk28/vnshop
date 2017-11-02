5fd846c123c2ad439eaeb5f8a00025d5bdc486eb
gkk28 <982596731@qq.com>
2017-11-02 09:44:20 +0800

    new text

--- client/src/components/Navbread.vue
+++ client/src/components/Navbread.vue
@@ -3,7 +3,7 @@
 		<div class="nav-breadcrumb-wrap">
 			<div class="container">
 				<nav class="nav-breadcrumb">
-					<a href="/">主页</a>
+					<a href="/">头部</a>
 					<slot></slot>
 				</nav> 	
 			</div>
